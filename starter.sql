c23dfce6cf56952f7b8ef835ca820798dc46dbd8
prabhak-rvs <prabhak@rvsgroup.com>
2026-07-30 14:29:57 +0530

    Create starter.sql for CollegeDB assignment
    Added initial SQL script for CollegeDB assignment with TODOs.

---
+++ starter.sql
@@ -0,0 +1,18 @@
+-- ===========================================
+-- CollegeDB Assignment
+-- Complete the TODO sections only.
+-- ===========================================
+
+-- TODO 1:
+-- Create a database named CollegeDB
+
+
+-- TODO 2:
+-- Use the CollegeDB database
+
+
+-- TODO 3:
+-- Create the Department table with the following fields:
+-- DepartmentID Number(5) Primary Key
+-- DepartmentName Varchar(20)
+-- HOD Varchar(20)
