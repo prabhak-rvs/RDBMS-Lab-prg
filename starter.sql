d438ba832b8ee7900f8a153138457ddb8bbcd644
prabhak-rvs <prabhak@rvsgroup.com>
2026-07-30 15:03:47 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -3,18 +3,14 @@
 -- Complete the TODO sections only.
 -- ===========================================
 
--- TODO 1:
--DROP DATABASE IF EXISTS CollegeDB;
+DROP DATABASE IF EXISTS CollegeDB;
 
 CREATE DATABASE CollegeDB;
 
+USE CollegeDB;
 
--- TODO 2:
--- Use the CollegeDB database
-
-
--- TODO 3:
--- Create the Department table with the following fields:
--- DepartmentID Number(5) Primary Key
--- DepartmentName Varchar(20)
--- HOD Varchar(20)
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(20),
+    HOD VARCHAR(20)
+);
